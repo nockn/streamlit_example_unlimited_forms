87e66d68c0220f5792686c5f7729515bc4ef35c6
nockn <shinshin.dra.96@gmail.com>
2022-11-05 09:24:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# How to Streamlit input form with interactive add/remove.
+This is a demo of Streamlit, which allows users to add/delete input forms.  
+This can be achieved by manipulating the loading location in st.container and combining st.session_state and for statements.  
+
+## Quick Start
+- pip(PyPl)  
+  > pip install streamlit
+
+- conda  
+  > conda install -c conda-forge streamlit
+
+> streamlit run streamlit.app
---
+++ streamlit_app.py
@@ -0,0 +1,77 @@
+import streamlit as st
+
+
+@st.experimental_memo
+def init_session_state():
+    st.session_state.list = [0]
+
+
+def app():
+    st.subheader("Demo: Streamlit input form with interactive add/remove.")
+
+    st.info("Sums the numbers entered.")
+
+    with st.form("form"):
+        st_input_area = st.container()
+        st_add_button_area = st.container()
+
+        if st_add_button_area.form_submit_button("ADD FORM"):
+            st.session_state.list.append(0)
+
+        if st_add_button_area.form_submit_button("REMOVE FORM"):
+            del st.session_state.list[-1]
+
+        for i in range(len(st.session_state.list)):
+            st.session_state.list[i] = st_input_area.number_input(f"Value {i+1}", step=1, key=i)
+
+        is_calc = st.form_submit_button("DONE")
+
+    if is_calc:
+        sum = 0
+        for _value in st.session_state.list:
+            sum += _value
+        st.success(f"Total: {sum}")
+    else:
+        pass  # DO NOTHING
+
+    source_code = """import streamlit as st
+
+st.set_page_config("Unlimited forms demo")
+
+if "list" not in st.session_state:
+    st.session_state.list = [0]
+
+st.info("Sums the numbers entered.")
+
+with st.form("form"):
+    st_input_area = st.container()
+    st_add_button_area = st.container()
+
+    if st_add_button_area.form_submit_button("ADD FORM"):
+        st.session_state.list.append(0)
+
+    if st_add_button_area.form_submit_button("REMOVE FORM"):
+        del st.session_state.list[-1]
+
+    for i in range(len(st.session_state.list)):
+        st.session_state.list[i] = st_input_area.number_input(f"Value {i+1}", step=1, key=i)
+
+    is_calc = st.form_submit_button("DONE")
+
+if is_calc:
+    sum = 0
+    for _value in st.session_state.list:
+        sum += _value
+    st.success(f"Total: {sum}")
+else:
+    pass  # DO NOTHING
+
+    """
+    with st.expander("sample code"):
+        st.code(source_code, language="python")
+
+
+if __name__ == "__main__":
+    st.set_page_config("Unlimited forms demo")
+    init_session_state()
+    app()
